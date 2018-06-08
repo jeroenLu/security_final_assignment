8f86efd998e1ed3ddf2373997ecd8900ecec3345
JEROEN\jeroen <jeroenluers@gmail.com>
2018-06-08 22:28:10 +0200

    adds workign post rout of secret

--- public/js/handler.js
+++ public/js/handler.js
@@ -3,20 +3,30 @@ $(function(){
 		var name = $("#name").val();
 		var text = $("#secretText").val();
         var password = $("#password").val();
-        
-        console.log(name)
-        console.log(text)
-        console.log(password)
+       
 		if(name.length > 0 && password.length > 0){
-			if(tekst.length > 0){
+			if(text.length > 0){
                 // if text field is filled -> send new secret
-				sendSecret(name,tekst,password);
+				sendSecret(name, text, password);
 			}else{
                 // if text feild is empty get the secret
-				getSecret(name,password);
+				getSecret(name, text);
 			}
 		}else{
 			alert("naam en/of wachtwoord niet ingevuld")
 		}
 	})
 })
+
+function sendSecret(name, text, password){
+	$.ajax({
+		url:"http://localhost:8080/secret",
+		method:"POST",
+		data:{name: name, secret:text, password:password},
+		success:function(data,status){
+			$("#geheimeTekst").val("");
+			$("#naam").val("");
+            $("#wachtwoord").val("");           
+		}
+	})
+}
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function (req, res, next) {    
+router.get('/', function (req, res, next) {
     res.render('index')
 });
+
+router.route("/secret")
+    .post(function (req, res) {
+        //initial data
+        username = req.body.name;
+        password = req.body.password;
+        secret = req.body.secret;
+        console.log(username)
+        console.log(password)
+        console.log(secret)
+
+        
+    })
 
 module.exports = router;
